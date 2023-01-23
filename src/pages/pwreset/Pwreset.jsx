de7ec42693b63b12680baa2579569d3caa922894
fireball90 <58610433+fireball90@users.noreply.github.com>
2023-01-23 17:39:06 +0100

    pwreset design kesz

--- src/pages/pwreset/Pwreset.jsx
+++ src/pages/pwreset/Pwreset.jsx
@@ -3,22 +3,29 @@ import '../pwreset/pwreset.css';
 
 export function Pwreset() {
   return <div className='d-flex justify-content-center align-items-center'>
-          <div className='pwreset-container'>
+          <div className='pwreset-container justify-content-center d-flex align-items-center'>
+            <div className='d-flex align-items-center flex-column'>
               <div className=''>
                   <img className='pwreset-img' alt='ISLANDERS' src='../images/islanders_logo.png'></img>
               </div>
+              <div className=''>
+                  <img className='pwreset-img2' alt='PWRESET' src='../images/elfelejtett_jelszo.png'></img>
+              </div>
                 <form id="form" className="">
-                  <div className="form-group row pb-3">
-                    <label className="col-form-label">REGISZTRÁLÁSNÁL HASZNÁLT EMAIL CÍM</label>
-                    <div className="col-sm-6">
-                      <input type="email" name="email" className="form-control" />
+                  <div className="justify-content-center form-group row pb-3">
+                    <label className="col-form-label text-center pwreset-label">REGISZTRÁLÁSNÁL HASZNÁLT EMAIL CÍM</label>
+                    <div className="col-sm-8">
+                      <input type="email" name="email" className="form-control pwreset-input" />
                     </div>
                   </div>
-                    <button type="submit" className='btn btn-button2'>Küldés</button>
+                  <div className='d-flex justify-content-center'>
+                    <button type="submit" className='btn btn-button-reset'>Küldés</button>
+                  </div>
                 </form>
               <div className=''>
-                  <p className='ml-auto'><a href='/register'>Visszatérés a login felületre</a></p>
+                  <p className='ml-auto pwreset-link'><a href='/login'>Visszatérés a bejelentkezéshez</a></p>
               </div>
+            </div>
           </div>
         </div>
      
