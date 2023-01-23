ac2e61b1c94e8045c45285f10faeaf8e96a4651c
fireball90 <58610433+fireball90@users.noreply.github.com>
2023-01-23 17:19:44 +0100

    register design kész

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,33 +5,35 @@ export function Register() {
   return <div className='d-flex justify-content-center align-items-center'>
           <div className='register-container justify-content-center d-flex align-items-center '>
             <div className='d-flex align-items-center flex-column'>
-              <div className=''>
                 <div className=''>
                     <img className='register-img' alt='ISLANDERS' src='../images/islanders_logo.png'></img>
                 </div>
+                <div className=''>
+                    <img className='register-img2' alt='REGISZTRÁCIÓ' src='../images/regisztracio.png'></img>
+                </div>
                 <form id="form" className="">
                   <div className="justify-content-center form-group row pb-3">
-                    <label className="col-form-label text-center">FELHASZNÁLÓ NÉV</label>
-                    <div className="col-sm-6">
-                      <input type="text" name="username" className="form-control" />
+                    <label className="col-form-label text-center register-label">FELHASZNÁLÓ NÉV</label>
+                    <div className="col-sm-8">
+                      <input type="text" name="username" className="form-control register-input" />
                     </div>
                   </div>
                   <div className="justify-content-center form-group row pb-3">
-                    <label className="col-form-label text-center">E-MAIL CÍM</label>
-                    <div className="col-sm-6">
-                      <input type="email" name="email" className="form-control" />
+                    <label className="col-form-label text-center register-label">E-MAIL CÍM</label>
+                    <div className="col-sm-8">
+                      <input type="email" name="email" className="form-control register-input" />
                     </div>
                   </div>
                   <div className="justify-content-center form-group row pb-3">
-                    <label className="col-form-label text-center">JELSZÓ</label>
-                    <div className="col-sm-6">
-                      <input type="password" name="password" className="form-control" />
+                    <label className="col-form-label text-center register-label">JELSZÓ</label>
+                    <div className="col-sm-8">
+                      <input type="password" name="password" className="form-control register-input" />
                     </div>
                   </div>
                   <div className="justify-content-center form-group row pb-3">
-                    <label className="col-form-label text-center">JELSZÓ ELLENŐRZÉSE</label>
-                    <div className="col-sm-6">
-                      <input type="password" name="pwcheck" className="form-control" />
+                    <label className="col-form-label text-center register-label">JELSZÓ ELLENŐRZÉSE</label>
+                    <div className="col-sm-8">
+                      <input type="password" name="pwcheck" className="form-control register-input" />
                     </div>
                   </div>
                   <div className='d-flex justify-content-center'>
@@ -39,9 +41,8 @@ export function Register() {
                   </div>
                 </form>
                 <div className='d-flex justify-content-center'>
-                    <p className='ml-auto'><a href='/register'>Már regisztrált? Lépjen be!</a></p>
+                    <p className='ml-auto register-link'><a href='/login'>Már regisztrált? Lépjen be!</a></p>
                 </div>
-              </div>
             </div>   
           </div>
         </div>
