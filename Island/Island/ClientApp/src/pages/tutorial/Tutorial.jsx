b86d4bb1f16200366deaa25d9b4eec31c8648447
Norbert Lekner <leknern@kkszki.hu>
2023-02-14 15:54:44 +0100

    backend added

---
+++ Island/Island/ClientApp/src/pages/tutorial/Tutorial.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Tutorial() {
+  return (
+    <div>    
+      <div className="d-flex align-items-center justify-content-center">
+        <div className="content-bg">
+          <p>Tutorial</p>
+        </div>
+      </div>
+    </div>
+  )
+     
+}
+
+
+export default Tutorial;
