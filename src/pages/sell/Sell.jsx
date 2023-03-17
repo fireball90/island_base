4d2da123c812b5da7d21a30cbbae2ba96058d33a
fireball90 <58610433+fireball90@users.noreply.github.com>
2023-03-18 00:20:04 +0100

    sell sort of working

--- src/pages/sell/Sell.jsx
+++ src/pages/sell/Sell.jsx
@@ -1,15 +1,55 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../sell/sell.css";
 import { Link } from "react-router-dom";
 import Layout from "../../components/layout/Layout";
 import HudContext from "../../contexts/HudContext";
+import axios from "axios";
 
 export default function Sell() {
   const { setIsHudDisplayed } = useContext(HudContext);
 
+  const [myItem, setMyItem] = useState("");
+  const [myItemAmount, setMyItemAmount] = useState(0);
+
+  const [theirItem, setTheirItem] = useState("");
+  const [theirItemAmount, setTheirItemAmount] = useState(0);
+
+  function myItemChangeHandler(event) {
+    setMyItem(event.target.value);
+  }
+  function myItemAmountChangeHandler(event) {
+    setMyItemAmount(event.target.value);
+  }
+
+  function theirItemChangeHandler(event) {
+    setTheirItem(event.target.value);
+  }
+  function theirItemAmountChangeHandler(event) {
+    setTheirItemAmount(event.target.value);
+  }
+
+
   useEffect(() => {
     setIsHudDisplayed(true);
   }, []);
+
+  function submit(event){
+    event.preventDefault();
+    console.log(myItem,myItemAmount,theirItem,theirItemAmount)
+    axios
+    .post("https://localhost:7276/api/Exchange/CreateExchange", {
+      item: myItem,
+      amount: myItemAmount,
+      replacementItem: theirItem,
+      replacementAmount: theirItemAmount,
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+    
+    });
+  }
 
   return (
     <Layout navigations={[]} title="Hirdetés feladása">
@@ -17,23 +57,26 @@ export default function Sell() {
         <div className="row">
           <div className="col-12 d-flex align-items-center justify-content-center flex-column sell-all">
             <h3>Hirdetés feladása</h3>
-            <form id="sell-form" className="row">
+            <form id="sell-form" className="row" onSubmit={submit}>
               <div className="justify-content-center">
                 <label className="col-sm-12 col-form-label text-center">
                   Válassz anyagot:
                 </label>
-                <select id="yourItems" name="yourItems" className="form-select">
-                  <option value="wood">Fa</option>
-                  <option value="stone">Kő</option>
-                  <option value="steel">Vas</option>
-                  <option value="coin">Érmék</option>
+                <select id="yourItems" name="myItem" className="form-select" value={myItem} onChange={myItemChangeHandler}>
+                  <option value="0">Fa</option>
+                  <option value="1">Kő</option>
+                  <option value="2">Vas</option>
+                  <option value="3">Érmék</option>
                 </select>
                 <label className="col-sm-12 col-form-label text-center">
                   Válassz mennyiséget:
                 </label>
                 <input
                   type="number"
-                  name="yourPiece"
+                  id="myAmount"
+                  name="myAmount"
+                  value={myItemAmount}
+                  onChange={myItemAmountChangeHandler}
                   className="form-control"
                 ></input>
               </div>
@@ -41,26 +84,25 @@ export default function Sell() {
                 <label className="col-sm-12 col-form-label text-center">
                   Mit kérsz cserébe:
                 </label>
-                <select
-                  id="theirItems"
-                  name="theirItems"
-                  className="form-select"
-                >
-                  <option value="wood">Fa</option>
-                  <option value="stone">Kő</option>
-                  <option value="steel">Vas</option>
-                  <option value="coin">Érmék</option>
+                <select id="theirItem" name="theirItem" className="form-select" value={theirItem} onChange={theirItemChangeHandler}>
+                  <option value="0">Fa</option>
+                  <option value="1">Kő</option>
+                  <option value="2">Vas</option>
+                  <option value="3">Érmék</option>
                 </select>
                 <label className="col-sm-12 col-form-label text-center">
                   Mennyit kérsz cserébe:
                 </label>
                 <input
                   type="number"
-                  name="theirPiece"
+                  name="theirAmount"
+                  id="theirAmount"
+                  value={theirItemAmount}
+                  onChange={theirItemAmountChangeHandler}
                   className="form-control"
                 ></input>
                 <div className="d-flex justify-content-center pt-3">
-                  <button className="sell-btn" onClick="">
+                  <button className="sell-btn" type="submit">
                     Hirdetés feladása
                   </button>
                 </div>
