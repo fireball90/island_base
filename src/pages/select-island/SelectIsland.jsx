7d1af32881e920c320a68d509df9bd041e5bebb0
szigilie@kkszki.hu <szigilie@kkszki.hu>
2023-03-26 19:17:59 +0200

    SelectIsland, Register apró módosítások.

--- src/pages/select-island/SelectIsland.jsx
+++ src/pages/select-island/SelectIsland.jsx
@@ -28,7 +28,7 @@ export default function SelectIsland() {
       )
       .then((response) => {
         setPlayer(response.data);
-        navigate("/island");
+        navigate("/notifications");
       })
       .catch(() => {
         alert("Nem sikerült kapcsolódni a szerverhez");
