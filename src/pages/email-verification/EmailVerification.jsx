e3f8c57aa8c47165f5ed7f19fb9b241fcb4e3ca7
Norbert Lekner <leknern@kkszki.hu>
2023-04-04 19:36:33 +0200

    x

--- src/pages/email-verification/EmailVerification.jsx
+++ src/pages/email-verification/EmailVerification.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 import HudContext from "../../contexts/HudContext";
 
