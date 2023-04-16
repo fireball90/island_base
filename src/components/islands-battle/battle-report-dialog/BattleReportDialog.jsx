edd8c26c62ecfa46fdb80c275bccd8de4269c7a8
Norbert Lekner <leknern@kkszki.hu>
2023-04-16 19:36:49 +0200

    finished battle dialog

--- src/components/islands-battle/battle-report-dialog/BattleReportDialog.jsx
+++ src/components/islands-battle/battle-report-dialog/BattleReportDialog.jsx
@@ -1,5 +1,4 @@
 import Modal from "react-bootstrap/Modal";
-import { Badge, Button } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
 
 import style from "./BattleReportDialog.module.css";
@@ -9,80 +8,85 @@ export default function BattleReportDialog({
   resetBattleReports,
 }) {
   return (
-    <Modal show={true} size="lg" centered>
+    <Modal show={true} size="lg" centered className={style.reportModal}>
       <div className={style.battleContainer}>
-      <Modal.Header className="border-0 text-white">
-        <div>
-          <Modal.Title>
-            {battleReports.isWinner
+        <Modal.Header className="border-0 text-white">
+          <div>
+            <h4>
+              {battleReports.isWinner
               ? "Gratulálunk, győztél!"
               : "Sajnáljuk, vesztettél!"}
-          </Modal.Title>
-          <div>Tekintsd meg a részletes csatajelentést.</div>
-        </div>
-      </Modal.Header>
-      <div className={style.battleReportContainer}>
-        <ListGroup className="rounded-0 text-white">
-          {battleReports.rounds.map((round, index) => (
-            <ListGroup.Item key={index} className="border-0">
+            </h4>
+          </div>
+        </Modal.Header>
+        <div className={style.battleReportContainer}>
+          <ListGroup className={`rounded-0 text-white ${style.reports}`}>
+            {battleReports.rounds.map((round, index) => (
+            <ListGroup.Item key={index} className="border-0 bg-transparent">
               <div className="d-flex mb-4">
                 <div className="w-25 d-flex align-items-center">
-                  <span className="fw-bold">{round.username}</span>
+                  <span className="text-white">{round.username}</span>
                 </div>
                 <div className="w-75">
                   <div className="d-flex justify-content-between w-100">
-                    <div>{round.message}</div>
-                    <div className="text-primary">{round.damage} sebzés</div>
+                    <div>
+                      <span className="text-white">{round.message}</span>
+                    </div>
+                    <div className="text-danger">
+                      <span>{round.damage} sebzés</span>
+                    </div>
                   </div>
-                  <div className="text-primary">
-                    {" "}
-                    Ellenfél fennmaradt életereje: {round.enemyRemainingHealth}
+                  <div className="text-warning">
+                    <span>Ellenfél fennmaradt életereje: {round.enemyRemainingHealth}</span>
                   </div>
                 </div>
               </div>
             </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </div>
-      <Modal.Body>
-        <div className="mt-3 text-white">
-          <div className="d-flex flex-row justify-content-around">
-            <div className="d-flex align-items-center">
-                Érmék: 
-              <Badge pill className="ms-1">
-                {battleReports.coins}
-              </Badge>
-            </div>
-            <div className="d-flex align-items-center">
-              Fa:
-              <Badge pill className="ms-1">
-                {battleReports.woods}
-              </Badge>
-            </div>
-            <div className="d-flex align-items-center">
-              Kövek:
-              <Badge pill className="ms-1">
-                {battleReports.stones}
-              </Badge>
-            </div>
-            <div className="d-flex align-items-center">
-              Vas:
-              <Badge pill className="ms-1">
-                {battleReports.irons}
-              </Badge>
-            </div>
-            <div className="d-flex align-items-center">
-              Tapasztalati pontok: 
-              <Badge pill className="ms-1">
-                {battleReports.experience}
-              </Badge>
+            ))}
+          </ListGroup>
+          <div className={`text-white ${style.items}`}>
+            <div className="d-flex flex-row justify-content-center gap-3">
+              <div className="d-flex align-items-center gap-1">
+                <img
+                  src="../images/icons/coin.png"
+                  alt="Érme"
+                  title="Érme"
+                ></img>
+                <span>{battleReports.coins}</span>
+              </div>
+              <div className="d-flex align-items-center gap-1">
+                <img src="../images/icons/wood.png" alt="Fa" title="Fa"></img>
+                <span>{battleReports.woods}</span>
+              </div>
+              <div className="d-flex align-items-center gap-1">
+                <img src="../images/icons/stone.png" alt="Kő" title="Kő"></img>
+                <span>{battleReports.stones}</span>
+              </div>
+              <div className="d-flex align-items-center gap-1">
+                <img
+                  src="../images/icons/steel.png"
+                  alt="Vas"
+                  title="Vas"
+                ></img>
+                <span>{battleReports.irons}</span>
+              </div>
+              <div className="d-flex align-items-center gap-1">
+                <img src="../images/icons/xp.png" alt="XP" title="XP"></img>
+                <span>{battleReports.experience}</span>
+              </div>
             </div>
           </div>
         </div>
-      </Modal.Body>
-      <Modal.Footer className="border-0">
-        <Button onClick={() => resetBattleReports()}>Rendben</Button>
-      </Modal.Footer>
+        <Modal.Footer className="border-0 d-flex justify-content-center">
+          <div>
+          <button
+                onClick={() => resetBattleReports()}
+                className={`font-btn ${style.closeButton}`}
+              >
+                Bezárás
+              </button>
+          </div>
+        </Modal.Footer>
       </div>
     </Modal>
   );
